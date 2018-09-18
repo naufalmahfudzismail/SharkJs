6cf314988471ada243726510940d09391b2a2fb9
Naufal <naufalmahfudzismail@gmail.com>
2018-09-19 06:55:23 +0700

    React with Bootsrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { SplitButton, MenuItem } from 'react-bootstrap';
 
 class App extends Component {
 
@@ -45,6 +46,9 @@ class App extends Component {
                </tbody>
             </table>
         </div>
+        <div>
+          <Boot/>
+        </div>
       </div>
     );
   }
@@ -60,6 +64,56 @@ class Mage extends Component{
             <td>{this.props.data.age}</td>
          </tr>
       </div>
+    );
+  }
+}
+
+class Boot extends Component{
+
+  state = { theme: null }
+  
+  chooseTheme = (theme, evt) => {
+    evt.preventDefault();
+    if (theme.toLowerCase() === 'reset') { theme = null }
+    this.setState({ theme });
+  }
+  render(){
+
+    const { theme } = this.state;
+    const themeClass = theme ? theme.toLowerCase() : 'default';
+    
+    const parentContainerStyles = {
+      position: 'absolute',
+      height: '100%',
+      width: '100%',
+      display: 'table'
+    };
+
+    const subContainerStyles = {
+      position: 'relative',
+      height: '100%',
+      width: '100%',
+      display: 'table-cell',
+      verticalAlign: 'middle'
+    };
+    return (
+      <div style={parentContainerStyles}>
+      <div style={subContainerStyles}>
+      
+        <span className={`h1 center-block text-center text-${theme ? themeClass : 'muted'}`} style={{ marginBottom: 25 }}>{theme || 'Default'}</span>
+        
+        <div className="center-block text-center">
+          <SplitButton bsSize="large" bsStyle={themeClass} title={`${theme || 'Default'} Theme`}>
+            <MenuItem eventKey="Primary" onSelect={this.chooseTheme}>Primary Theme</MenuItem>
+            <MenuItem eventKey="Danger" onSelect={this.chooseTheme}>Danger Theme</MenuItem>
+            <MenuItem eventKey="Success" onSelect={this.chooseTheme}>Success Theme</MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey="Reset" onSelect={this.chooseTheme}>Default Theme</MenuItem>
+          </SplitButton>
+        </div>
+        
+      </div>
+    </div>
     );
   }
 }
