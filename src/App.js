087f8d70598bdcc41e87b509c47b369929007986
Naufal <naufalmahfudzismail@gmail.com>
2018-09-20 04:54:35 +0700

    React with threeJs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { SplitButton, MenuItem } from 'react-bootstrap';
+import * as THREE from 'three';
 
 class App extends Component {
 
@@ -48,6 +49,9 @@ class App extends Component {
         </div>
         <div>
           <Boot/>
+        </div>
+        <div className="center-block text-center">
+          <Threejs/>
         </div>
       </div>
     );
@@ -117,5 +121,64 @@ class Boot extends Component{
     );
   }
 }
+
+class Threejs extends Component{
+
+      componentDidMount(){
+        const width = this.mount.clientWidth
+        const height = this.mount.clientHeight
+        //ADD SCENE
+        this.scene = new THREE.Scene()
+        //ADD CAMERA
+        this.camera = new THREE.PerspectiveCamera(
+          75,
+          width / height,
+          0.1,
+          1000
+        )
+        this.camera.position.z = 4
+        //ADD RENDERER
+        this.renderer = new THREE.WebGLRenderer({ antialias: true })
+        this.renderer.setClearColor('#000000')
+        this.renderer.setSize(width, height)
+        this.mount.appendChild(this.renderer.domElement)
+        //ADD CUBE
+        const geometry = new THREE.BoxGeometry(1, 1, 1)
+        const material = new THREE.MeshBasicMaterial({ color: '#433F81'     })
+        this.cube = new THREE.Mesh(geometry, material)
+        this.scene.add(this.cube)
+    this.start()
+      }
+
+    componentWillUnmount(){
+        this.stop()
+        this.mount.removeChild(this.renderer.domElement)
+      }
+    start = () => {
+        if (!this.frameId) {
+          this.frameId = requestAnimationFrame(this.animate)
+        }
+      }
+    stop = () => {
+        cancelAnimationFrame(this.frameId)
+      }
+    animate = () => {
+      this.cube.rotation.x += 0.01
+      this.cube.rotation.y += 0.01
+      this.renderScene()
+      this.frameId = window.requestAnimationFrame(this.animate)
+    }
+    renderScene = () => {
+      this.renderer.render(this.scene, this.camera)
+    }
+    render(){
+        return(
+          <div
+            style={{ width: '800px', height: '800px' }}
+            ref={(mount) => { this.mount = mount }}
+          ></div>
+        );
+      }
+    }
 
 export default App;
