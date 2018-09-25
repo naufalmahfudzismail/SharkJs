ae3eff6ea7ddcf34e13acd67102a6655e8e25e84
Naufal <naufalmahfudzismail@gmail.com>
2018-09-26 05:34:18 +0700

    Clean Code

--- src/App.js
+++ src/App.js
@@ -1,139 +1,153 @@
-import React, { Component } from 'react';
-import './App.css';
-import { SplitButton, MenuItem } from 'react-bootstrap';
-import * as THREE from 'three';
-import Grid from './components/grid';
-import MoveAble from './components/MoveAble';
+import React, { Component } from "react";
+import "./App.css";
+import { SplitButton, MenuItem } from "react-bootstrap";
+import * as THREE from "three";
+import Grid from "./components/grid";
+import MoveAble from "./components/MoveAble";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <div >
-          <Boot/>
+        <div>
+          <Boot />
         </div>
-        <div >
-          <Threejs/>
+        <div>
+          <Threejs />
         </div>
-        <div >
-          <Grid/>
+        <div>
+          <Grid />
         </div>
-        <div >
-          <MoveAble/>
+        <div>
+          <MoveAble />
         </div>
       </div>
     );
   }
 }
 
-class Boot extends Component{
+class Boot extends Component {
+  state = { theme: null };
 
-  state = { theme: null }
-  
   chooseTheme = (theme, evt) => {
     evt.preventDefault();
-    if (theme.toLowerCase() === 'reset') { theme = null }
+    if (theme.toLowerCase() === "reset") {
+      theme = null;
+    }
     this.setState({ theme });
-  }
-  render(){
-
+  };
+  render() {
     const { theme } = this.state;
-    const themeClass = theme ? theme.toLowerCase() : 'default';
-    
+    const themeClass = theme ? theme.toLowerCase() : "default";
+
     const parentContainerStyles = {
-      position: 'absolute',
-      height: '100%',
-      width: '100%',
-      display: 'table'
+      position: "absolute",
+      height: "100%",
+      width: "100%",
+      display: "table"
     };
 
     const subContainerStyles = {
-      position: 'relative',
-      height: '100%',
-      width: '100%',
-      display: 'table-cell',
-      verticalAlign: 'middle'
+      position: "relative",
+      height: "100%",
+      width: "100%",
+      display: "table-cell",
+      verticalAlign: "middle"
     };
     return (
       <div style={parentContainerStyles}>
-      <div style={subContainerStyles}>
-      
-        <span className={`h1 center-block text-center text-${theme ? themeClass : 'muted'}`} style={{ marginBottom: 25 }}>{theme || 'Default'}</span>
-        
-        <div className="center-block text-center">
-          <SplitButton bsSize="large" bsStyle={themeClass} title={`${theme || 'Default'} Theme`}>
-            <MenuItem eventKey="Primary" onSelect={this.chooseTheme}>Primary Theme</MenuItem>
-            <MenuItem eventKey="Danger" onSelect={this.chooseTheme}>Danger Theme</MenuItem>
-            <MenuItem eventKey="Success" onSelect={this.chooseTheme}>Success Theme</MenuItem>
-            <MenuItem divider />
-            <MenuItem eventKey="Reset" onSelect={this.chooseTheme}>Default Theme</MenuItem>
-          </SplitButton>
+        <div style={subContainerStyles}>
+          <span
+            className={`h1 center-block text-center text-${
+              theme ? themeClass : "muted"
+            }`}
+            style={{ marginBottom: 25 }}
+          >
+            {theme || "Default"}
+          </span>
+
+          <div className="center-block text-center">
+            <SplitButton
+              bsSize="large"
+              bsStyle={themeClass}
+              title={`${theme || "Default"} Theme`}
+            >
+              <MenuItem eventKey="Primary" onSelect={this.chooseTheme}>
+                Primary Theme
+              </MenuItem>
+              <MenuItem eventKey="Danger" onSelect={this.chooseTheme}>
+                Danger Theme
+              </MenuItem>
+              <MenuItem eventKey="Success" onSelect={this.chooseTheme}>
+                Success Theme
+              </MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey="Reset" onSelect={this.chooseTheme}>
+                Default Theme
+              </MenuItem>
+            </SplitButton>
+          </div>
         </div>
-        
       </div>
-    </div>
     );
   }
 }
 
 class Threejs extends Component {
-      componentDidMount(){
-        
-          const width = window.innerWidth
-          const height = window.innerHeight
-          //const aspectRatio = this.width / this.height
-          //ADD SCENE
-          this.scene = new THREE.Scene()
-          //ADD CAMERA
-          this.camera = new THREE.PerspectiveCamera(
-            50, width / height, 1, 1000
-          )
-          this.camera.position.z = 5
-          //this.camera.aspect = aspectRatio;
-          //ADD RENDERER
-          this.renderer = new THREE.WebGLRenderer( { antialias : true} )
-          this.renderer.setClearColor('#99ff99')
-          this.renderer.setSize(width, height)
-          this.camera.updateProjectionMatrix();
-          this.mount.appendChild(this.renderer.domElement)
-          //ADD CUBE
-          const geometry = new THREE.BoxGeometry(1, 1, 1)
-          const material = new THREE.MeshBasicMaterial({ color: '#433F81'     })
-          this.cube = new THREE.Mesh(geometry, material)
-          this.scene.add(this.cube)
-          this.start()
-        }
+  componentDidMount() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    //const aspectRatio = this.width / this.height
+    //ADD SCENE
+    this.scene = new THREE.Scene();
+    //ADD CAMERA
+    this.camera = new THREE.PerspectiveCamera(50, width / height, 1, 1000);
+    this.camera.position.z = 5;
+    //this.camera.aspect = aspectRatio;
+    //ADD RENDERER
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setClearColor("#99ff99");
+    this.renderer.setSize(width, height);
+    this.camera.updateProjectionMatrix();
+    this.mount.appendChild(this.renderer.domElement);
+    //ADD CUBE
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: "#433F81" });
+    this.cube = new THREE.Mesh(geometry, material);
+    this.scene.add(this.cube);
+    this.start();
+  }
 
-      componentWillUnmount(){
-          this.stop()
-          this.mount.removeChild(this.renderer.domElement)
-        }
-      start = () => {
-          if (!this.frameId) {
-            this.frameId = requestAnimationFrame(this.animate)
-          }
-        }
-      stop = () => {
-          cancelAnimationFrame(this.frameId)
-        }
-      animate = () => {
-        this.cube.rotation.x += 0.05
-        this.cube.rotation.y += 0.05
-        this.renderScene()
-        this.frameId = window.requestAnimationFrame(this.animate)
-      }
-      renderScene = () => {
-        this.renderer.render(this.scene, this.camera)
-      }
-        render(){
-            return(
-              <div
-                ref={(mount) => { this.mount = mount }}
-              ></div>
-            );
-          }
+  componentWillUnmount() {
+    this.stop();
+    this.mount.removeChild(this.renderer.domElement);
+  }
+  start = () => {
+    if (!this.frameId) {
+      this.frameId = requestAnimationFrame(this.animate);
     }
-
-    
+  };
+  stop = () => {
+    cancelAnimationFrame(this.frameId);
+  };
+  animate = () => {
+    this.cube.rotation.x += 0.05;
+    this.cube.rotation.y += 0.05;
+    this.renderScene();
+    this.frameId = window.requestAnimationFrame(this.animate);
+  };
+  renderScene = () => {
+    this.renderer.render(this.scene, this.camera);
+  };
+  render() {
+    return (
+      <div
+        ref={mount => {
+          this.mount = mount;
+        }}
+      />
+    );
+  }
+}
 
 export default App;
--- src/components/MoveAble.js
+++ src/components/MoveAble.js
@@ -1,105 +1,113 @@
-import * as THREE from 'three';
-import React, { Component } from 'react';
-import Detector from '../controls/Detector';
-import DragControls from 'three-dragcontrols';
-import OrbitControls from 'three-orbit-controls';
+import * as THREE from "three";
+import React, { Component } from "react";
+import Detector from "../controls/Detector";
+import DragControls from "three-dragcontrols";
+import OrbitControls from "three-orbit-controls";
 
 var camera, scene, renderer, dragControls, controls;
 
-class MoveAble extends Component{
+class MoveAble extends Component {
+  componentDidMount() {
+    if (!Detector.webgl) Detector.addGetWebGLMessage();
+    const objects = [];
 
-    componentDidMount(){
+    this.container = document.createElement("div");
+    document.body.appendChild(this.container);
 
-        if ( !Detector.webgl ) Detector.addGetWebGLMessage();
+    //set the camera
+    camera = new THREE.PerspectiveCamera(
+      60,
+      window.innerWidth / window.innerHeight,
+      1,
+      10000
+    );
+    camera.position.set(400, 200, 0);
 
-       
-        const objects = [];
-        
-                this.container = document.createElement( 'div' );
-                document.body.appendChild( this.container );
-                
-				//set the camera
-				camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 10000);
-                camera.position.set(400, 200, 0);
-                
-				//set the control so you can rotating the scene
-                controls = new OrbitControls(camera);
-				controls.enableDamping = true;  // to create an inertia effect when camera is moving. an animation loop is required when either damping or auto-rotation are enabled
-				controls.dampingFactor = 0.25; // set the factor of damping
-				controls.screenSpacePanning = false;
-				controls.minDistance = 100; // how far the camera will zoom out
-				controls.maxDistance = 500; // how near the camera will zoom in
-                controls.maxPolarAngle = Math.PI / 2;
-                
-				// Create the Scene
-				scene = new THREE.Scene();
-				scene.background = new THREE.Color(0xcccccc);
-                scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
-                
-                //light effect for the mesh
-				const light1 = new THREE.DirectionalLight( 0xffffff );
-                light1.position.set( 1, 1, 1 );
-                scene.add( light1 );
-                
-				const light2 = new THREE.DirectionalLight( 0x002288 );
-				light2.position.set( - 1, - 1, - 1 );
-                scene.add( light2 );
-                
-				const light3 = new THREE.AmbientLight( 0x222222 );
-                scene.add( light3 );
-                
-				// set the movable object
-				const geometry = new THREE.CylinderBufferGeometry( 0, 10, 30, 4, 1 );
-                const material = new THREE.MeshPhongMaterial( { color: 0xffffff, flatShading: true } );
-                
-				for ( var i = 0; i < 500; i ++ ) {
-					var mesh = new THREE.Mesh( geometry, material );
-					mesh.position.x = Math.random() * 1600 - 800;
-					mesh.position.y = Math.random() * 1600 - 800;
-					mesh.position.z = Math.random() * 1600 - 800;
-					scene.add(mesh);
-					objects.push(mesh);
+    //set the control so you can rotating the scene
+    controls = new OrbitControls(camera);
+    controls.enableDamping = true; // to create an inertia effect when camera is moving. an animation loop is required when either damping or auto-rotation are enabled
+    controls.dampingFactor = 0.25; // set the factor of damping
+    controls.screenSpacePanning = false;
+    controls.minDistance = 100; // how far the camera will zoom out
+    controls.maxDistance = 500; // how near the camera will zoom in
+    controls.maxPolarAngle = Math.PI / 2;
 
-                 }
+    // Create the Scene
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xcccccc);
+    scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
 
-                renderer = new THREE.WebGLRenderer({antialias: true});
-				renderer.setPixelRatio(window.devicePixelRatio);
-				renderer.setSize(window.innerWidth, window.innerHeight);
-				renderer.setClearColor("#2E2B40");
-                this.container.appendChild(renderer.domElement);
-                
-				//add the drag controller so the item can be drag
-                dragControls = new DragControls(objects, camera, renderer.domElement);
-                
-                dragControls.addEventListener('dragstart', function ( event )
-                 { this.controls.enabled = false; }); //when start drag, stop rotate when click
-                dragControls.addEventListener('dragend', function ( event )
-                 { this.controls.enabled = true; }); // enable rotate click when drag event end
-                //set event listener for resizing the scene
-            
-				window.addEventListener('resize', function() {
-					var WIDTH = window.innerWidth,
-			            HEIGHT = window.innerHeight;
-			        renderer.setSize(WIDTH, HEIGHT);
-			        camera.aspect = WIDTH / HEIGHT;
-			        camera.updateProjectionMatrix();
-				});
+    //light effect for the mesh
+    const light1 = new THREE.DirectionalLight(0xffffff);
+    light1.position.set(1, 1, 1);
+    scene.add(light1);
 
-                 this.animate();
+    const light2 = new THREE.DirectionalLight(0x002288);
+    light2.position.set(-1, -1, -1);
+    scene.add(light2);
+
+    const light3 = new THREE.AmbientLight(0x222222);
+    scene.add(light3);
+
+    // set the movable object
+    const geometry = new THREE.CylinderBufferGeometry(0, 10, 30, 4, 1);
+    const material = new THREE.MeshPhongMaterial({
+      color: 0xffffff,
+      flatShading: true
+    });
+
+    for (var i = 0; i < 500; i++) {
+      var mesh = new THREE.Mesh(geometry, material);
+      mesh.position.x = Math.random() * 1600 - 800;
+      mesh.position.y = Math.random() * 1600 - 800;
+      mesh.position.z = Math.random() * 1600 - 800;
+      scene.add(mesh);
+      objects.push(mesh);
     }
 
-    animate = () =>{
-        requestAnimationFrame( this.animate );
-				controls.update();  //only required if controls.enableDamping = true, or if controls.autoRotate = true
-				renderer.render( this.scene, this.camera );
-    }
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor("#2E2B40");
+    this.container.appendChild(renderer.domElement);
 
-    render(){
-        return(
-            <div   ref={(mount) => { this.mount = mount }}>  
-            </div>
-        )
-    }
+    //add the drag controller so the item can be drag
+    dragControls = new DragControls(objects, camera, renderer.domElement);
+
+    dragControls.addEventListener("dragstart", function(event) {
+      this.controls.enabled = false;
+    }); //when start drag, stop rotate when click
+    dragControls.addEventListener("dragend", function(event) {
+      this.controls.enabled = true;
+    }); // enable rotate click when drag event end
+    //set event listener for resizing the scene
+
+    window.addEventListener("resize", function() {
+      var WIDTH = window.innerWidth,
+        HEIGHT = window.innerHeight;
+      renderer.setSize(WIDTH, HEIGHT);
+      camera.aspect = WIDTH / HEIGHT;
+      camera.updateProjectionMatrix();
+    });
+
+    this.animate();
+  }
+
+  animate = () => {
+    requestAnimationFrame(this.animate);
+    controls.update(); //only required if controls.enableDamping = true, or if controls.autoRotate = true
+    renderer.render(this.scene, this.camera);
+  };
+
+  render() {
+    return (
+      <div
+        ref={mount => {
+          this.mount = mount;
+        }}
+      />
+    );
+  }
 }
 
 export default MoveAble;
